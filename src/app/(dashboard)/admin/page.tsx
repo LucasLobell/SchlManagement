8105a9251c03757c403abe859cf5072aaef7cdb8
Lucas Lobell <lucaslobell2@gmail.com>
2024-08-29 11:43:40 -0300

    starting student page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import Announcements from '@/components/Announcements'
 import AttendenceChart from '@/components/AttendenceChart'
 import CountChart from '@/components/CountChart'
 import EventCalendar from '@/components/EventCalendar'
@@ -29,6 +30,7 @@ const AdminPage = () => {
       </div>
       <div className='w-full lg:w-1/3 flex flex-col gap-8'>
         <EventCalendar />
+        <Announcements />
       </div>
     </div>
   )
